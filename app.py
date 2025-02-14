4e01587e942494892b052a3840f22acd0bfc460e
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2025-02-13 18:00:06 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,22 @@ Created on Thu Feb 13 17:51:26 2025
 
 import streamlit as st
 
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 # Título de la aplicación
 st.title("Día del Amor y la Amistad - PENSIONISSSTE")
 
